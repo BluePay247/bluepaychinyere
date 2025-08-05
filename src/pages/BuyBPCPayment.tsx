e617aeaab604ba54f0bce94c54f2a4126d36fb7d
BluePay247 <bluepaycompany247@gmail.com>
2025-08-05 13:37:59 +0100

    Update BuyBPCPayment.tsx

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -116,12 +116,12 @@ const BuyBPCPayment = () => {
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-lg font-bold">5071766430</p>
+            <p className="text-lg font-bold">7089518688</p>
             <Button
               variant="default"
               size="sm"
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-1"
-              onClick={() => handleCopy("5071766430", "Account Number")}
+              onClick={() => handleCopy("7089518688", "Account Number")}
             >
               <Copy size={14} />
               Copy
@@ -131,12 +131,12 @@ const BuyBPCPayment = () => {
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Bank Name</p>
-          <p className="text-lg font-bold">MONIEPOINT</p>
+          <p className="text-lg font-bold">PALMPAY</p>
         </div>
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Name</p>
-          <p className="text-lg font-bold">BRIDGET LAMIDI</p>
+          <p className="text-lg font-bold">DESTINY CHISOM CHINYERE</p>
         </div>
       </div>
 
